e8375fabc0aec57d04c0320b8a780dac3a7c84c7
ViktorChizh <victor.chizh@mail.ru>
2025-11-07 13:32:59 +0300

    learn modules

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let num = 5
-console.log(`Variable: ${num}`)
+const os = require('os')
+let res = os.platform()
+console.log(res)
+
+const my_math = require('./my_math')
+
+console.log (my_math.add(5,6))
+console.log (my_math.minus(15,6))
