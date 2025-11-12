886d603eb1d23fd86f215dab7ed56a65d4540ed2
ViktorChizh <victor.chizh@mail.ru>
2025-11-12 16:30:11 +0300

    working with template engine

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('This home page')
+    // res.sendFile(__dirname + '/index.html') // без шаблонизатора ejs
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.send('This about page')
+    // res.send('This about page') // без шаблонизатора ejs
+    res.render('about')
 })
 
 app.get('/user/:username/:id', (req, res) => {
-    res.send(`User ID: ${req.params.id} Username: ${req.params.username}`)
+    // res.send(`User ID: ${req.params.id} Username: ${req.params.username}`) // без шаблонизатора ejs
+    let data = {
+        username: req.params.username,
+        id: req.params.id,
+        hobbies: ['Football', 'Skate', 'Basketball']
+    }
+    res.render('user', data)
 })
 
 const PORT = 3000
