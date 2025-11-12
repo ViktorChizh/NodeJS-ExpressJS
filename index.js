00994cea35cc7b5fdbe827d98caa2a50c8d605be
ViktorChizh <victor.chizh@mail.ru>
2025-11-12 15:23:44 +0300

    working with url

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+    res.send('This home page')
+})
 
+app.get('/about', (req, res) => {
+    res.send('This about page')
+})
+
+app.get('/user/:username/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id} Username: ${req.params.username}`)
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`server started: http://localhost:${PORT}`)
 })
